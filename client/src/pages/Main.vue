5c61d2869365a262b02a3ed40e5412483ce792ff
Jose Naranjo <naranjja@gmail.com>
2018-12-28 14:26:04 -0500

    :sparkles: Add navbar

--- client/src/pages/Main.vue
+++ client/src/pages/Main.vue
@@ -1,14 +1,19 @@
 <template>
-<div class="container">
-    <tiles />
+<div>
+    <navbar />
+    <div class="container">
+        <tiles />
+    </div>
 </div>
 </template>
 
 <script>
+import Navbar from "./../components/Navbar"
 import Tiles from "./../components/Tiles"
 export default {
     name: "Main",
     components: {
+        navbar: Navbar,
         tiles: Tiles,
     }
 }
