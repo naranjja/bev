7ef85950723a7feea0630f2ca965f61b2b10a196
Jose Naranjo <naranjja@gmail.com>
2018-12-28 14:20:47 -0500

    :construction: Restructure pages/components

---
+++ client/src/pages/Main.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="container">
+    <tiles />
+</div>
+</template>
+
+<script>
+import Tiles from "./../components/Tiles"
+export default {
+    name: "Main",
+    components: {
+        tiles: Tiles,
+    }
+}
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue"
+import Router from "vue-router"
+import Main from "@/pages/Main"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "Main",
+      component: Main
     }
   ]
 })
